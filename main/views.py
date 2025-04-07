abd214564a59dd39679251222450cff467663167
onowoey <cg10046@naver.com>
2025-04-08 00:01:21 +0900

    2주차 과제3

--- main/views.py
+++ main/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def mainpage(request):
-    return render(request, 'main/mainpage.html')
+    context = {
+        "generation": 13,
+        "info": {'variable': '변수', 'tag': '태그', 'filter': '필터'},
+    }
+    return render(request, 'main/mainpage.html', context)
+
 def secondpage(request):
-    return render(request,'main/secondpage.html')
+    features = [
+        "입천장이 까맣다",
+        "고구마를 너무 너무 좋아해서 5키로가 넘은 적이 있음",
+        "산책을 싫어해서 절대 걷지 않고 안겨서 감",
+        "혼자 있으면 화장실도 잘 안 감",
+        "최근에 털을 빡빡 깎았음",
+    ]
+    return render(request, 'main/secondpage.html', {'features': features})
